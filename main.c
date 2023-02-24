fd7dfba9dccbb58d34eb99511cd18254daba5b44
Simon <simon.ruskola@gmail.com>
2023-02-24 11:14:59 +0100

    setup

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * d0003e_labb5.c
+ *
+ * Created: 2023-02-24 11:10:07
+ * Author : simon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
